9958ef9aa0330b1400c53ad20237f75e53b8450c
BEKIR MUGAYITOGLU <bmugayitoglu@gmail.com>
2019-12-15 22:50:49 -0800

    Add files via upload

---
+++ chatbot2.py
@@ -0,0 +1,17 @@
+print("Hello. I am a social person and a chatbot. I am here to spend time with you if you are available.")
+print("Let me introduce myself. I like playing soccer and I love to talk about about computer science education.")
+name = input("What is your name?: ")
+print("Hello", name, ", Nice to meet you!")
+
+print("I love coffee and I also like trying out new kinds of food.")
+food = input("How about you? What is your favorite food?: ")
+print('I like', food, 'too.')
+question = 'How often do you eat ' + food + '?:'
+howoften = input(question)
+print('Interesting. I wonder if that is good for your health.')
+animal = input("My favorite animal is a cat. What is yours?: ")
+print('I wonder if a', animal, 'likes to eat', food, '?')
+
+feeling = input("How are you feeling today?: ")
+reason = input("Please tell me more about why you feel like that: ")
+print("I understand. Thanks for sharing. Don't hesitate to contact me if you want to spend time. Have a great day my new friend.")
